0f79576e820aeaf0a753b2befbc34fa10817fcc7
vakas-786 <vakas0959@gmail.com>
2020-10-18 19:35:40 -0400

    fixed href

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -16,9 +16,11 @@ export const Intro = () => {
         <Details theme={theme}>
           <h1>Hi There!</h1>
           <h4>I’m Vakas and I’m a Full Stack Developer!</h4>
-          <Button as={AnchorLink} href="https://docs.google.com/document/d/1eKIJ5QTHlcFvCKLc4t1vQ_dz_SqQIc5QFExF-TwYz38/edit?usp=sharing">
+          <a href="https://docs.google.com/document/d/1eKIJ5QTHlcFvCKLc4t1vQ_dz_SqQIc5QFExF-TwYz38/edit?usp=sharing">
+          <Button >
             View Resume
           </Button>
+          </a>
         </Details>
         <Thumbnail>
           <img src={dev} alt="I’m Vakas and I’m a Full Stack Developer!" />
