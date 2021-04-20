0d387f1233b63719dda0059d502a9062d4b16f02
vakas-786 <vakas0959@gmail.com>
2021-04-20 15:10:37 -0400

    remove helmet gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,6 @@ module.exports = {
     author: config.author,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
